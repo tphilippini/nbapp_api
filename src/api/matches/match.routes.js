c314b852ea4d6b61c4f3181767df77c609e50389
tphilippini <t.philippini@hotmail.fr>
2023-08-07 22:08:07 +0200

    feat: adds auth middleware

--- src/api/matches/match.routes.js
+++ src/api/matches/match.routes.js
@@ -2,14 +2,14 @@
 
 import { Router } from 'express';
 import matchController from '@/api/matches/match.controller';
-// import userGuardMidd from '@/middlewares/userGuard';
 import { regex } from '@/helpers/validator';
+import userGuardMidd from '@/middlewares/userGuard';
 
 const matchRouter = Router();
 
 matchRouter.get(
   `/:startDate(${regex.date.yyyymmdd})`,
-  // userGuardMidd,
+  userGuardMidd,
   matchController.matchByDate
 );
 
--- src/config/server.js
+++ src/config/server.js
@@ -3,23 +3,22 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import expressJwt from 'express-jwt';
 import helmet from 'helmet';
 import mongoose from 'mongoose';
-import date from '@/helpers/date';
+import authErrorMidd from '@/middlewares/authError';
+import authRouter from '@/api/auth/auth.routes';
 // import leagueRouter from '@/api/leagues/league.routes';
-// import corsMidd from '@/middlewares/cors';
+import corsMidd from '@/middlewares/cors';
+import date from '@/helpers/date';
 import log from '@/helpers/log';
 import matchRouter from '@/api/matches/match.routes';
-import authRouter from '@/api/auth/auth.routes';
-// import expressJwt from 'express-jwt';
 import otherMidd from '@/middlewares/other';
+import passport from '@/config/passport';
 import response from '@/helpers/response';
 // import deviceRouter from '@/api/devices/device.routes';
 import userRouter from '@/api/users/user.routes';
 
-// import passport from '@/config/passport';
-
-// import authErrorMidd from '@/middlewares/authError';
 // import '@/scripts/cron';
 
 const app = express();
@@ -31,7 +30,7 @@ class Server {
     app.use(cors());
 
     // CORS middleware
-    // app.use(corsMidd);
+    app.use(corsMidd);
 
     // A simple middleware
     app.use(otherMidd);
@@ -46,32 +45,32 @@ class Server {
     );
 
     // Password auth
-    // app.use(passport.initialize());
+    app.use(passport.initialize());
 
     // Auth middleware
-    // app.use(
-    //   expressJwt({
-    //     secret: process.env.API_ACCESS_TOKEN_SECRET,
-    //   }).unless({
-    //     path: [
-    //       {
-    //         url: `${process.env.API_VERSION}/users`,
-    //         methods: ['OPTIONS', 'POST'],
-    //       },
-    //       `${process.env.API_VERSION}/auth`,
-    //       `${process.env.API_VERSION}/auth/token`,
-    //       // `${process.env.API_VERSION}/auth/google/token`,
-    //       // `${process.env.API_VERSION}/auth/facebook/token`,
-    //       `${process.env.API_VERSION}/auth/forgot`,
-    //       `${process.env.API_VERSION}/auth/reset`,
-    //       `${process.env.API_VERSION}/auth/validate`,
-    //       // `${process.env.API_VERSION}/users/test`,
-    //     ],
-    //   })
-    // );
+    app.use(
+      expressJwt({
+        secret: process.env.API_ACCESS_TOKEN_SECRET,
+      }).unless({
+        path: [
+          {
+            url: `${process.env.API_VERSION}/users`,
+            methods: ['OPTIONS', 'POST'],
+          },
+          `${process.env.API_VERSION}/auth`,
+          `${process.env.API_VERSION}/auth/token`,
+          // `${process.env.API_VERSION}/auth/google/token`,
+          // `${process.env.API_VERSION}/auth/facebook/token`,
+          `${process.env.API_VERSION}/auth/forgot`,
+          `${process.env.API_VERSION}/auth/reset`,
+          `${process.env.API_VERSION}/auth/validate`,
+          // `${process.env.API_VERSION}/users/test`,
+        ],
+      })
+    );
 
     // Middleware to handle error from authentication
-    // app.use(authErrorMidd);
+    app.use(authErrorMidd);
 
     log.title('Initialization');
     log.success(`The current env is ${process.env.NODE_ENV}`);
