263ef08adee85232b1edb74add158aa9f3f58d4b
tphilippini <t.philippini@hotmail.fr>
2023-08-07 18:15:54 +0200

    Updates logs

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -1,52 +1,39 @@
 'use strict';
 
-import logger from './logger';
+// import logger from './logger';
 
 const log = {};
 
-log.success = (value) => {
-  if (process.env.NODE_ENV !== 'production') {
-    return console.log('\x1b[32m✔ %s\x1b[0m', value);
-  }
-  return logger.info(value);
-};
+log.success = (value) => console.log('\x1b[32m✔ %s\x1b[0m', value);
+// if (process.env.NODE_ENV !== 'production') {
+// }
+// return logger.info(value);
 
-log.info = (value) => {
-  if (process.env.NODE_ENV !== 'production') {
-    return console.info('\x1b[36m➡ %s\x1b[0m', value);
-  }
-  return logger.info(value);
-};
+log.info = (value) => console.info('\x1b[36m➡ %s\x1b[0m', value);
+// if (process.env.NODE_ENV !== 'production') {
+// }
+// return logger.info(value);
 
-log.error = (value) => {
-  if (process.env.NODE_ENV !== 'production') {
-    return console.error('\x1b[31m✖ %s\x1b[0m', value);
-  }
-  return logger.error(value);
-};
+log.error = (value) => console.error('\x1b[31m✖ %s\x1b[0m', value);
+// if (process.env.NODE_ENV !== 'production') {
+// }
+// return logger.error(value);
 
-log.warning = (value) => {
-  if (process.env.NODE_ENV !== 'production') {
-    return console.warn('\x1b[33m❗ %s\x1b[0m', value);
-  }
-  return logger.warn(value);
-};
+log.warning = (value) => console.warn('\x1b[33m❗ %s\x1b[0m', value);
+// if (process.env.NODE_ENV !== 'production') {
+// }
+// return logger.warn(value);
 
-log.title = (value) => {
-  if (process.env.NODE_ENV !== 'production') {
-    return console.log(
-      '\n---\n\n\x1b[7m.: %s :.\x1b[0m\n',
-      value.toUpperCase()
-    );
-  }
-  return logger.info(value);
-};
+log.title = (value) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  console.log('\n---\n\n\x1b[7m.: %s :.\x1b[0m\n', value.toUpperCase());
+// if (process.env.NODE_ENV !== 'production') {
+// }
+// return logger.info(value);
 
-log.default = (value) => {
-  if (process.env.NODE_ENV !== 'production') {
-    return console.log('%s', value);
-  }
-  return logger.info(value);
-};
+log.default = (value) => console.log('%s', value);
+// if (process.env.NODE_ENV !== 'production') {
+// }
+// return logger.info(value);
 
 export default log;
